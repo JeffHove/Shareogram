5c635200bc625877575d952a2e2e78ae6b9d2834
Jefferson Hovey <jefferson.hovey@gmail.com>
2025-02-21 03:26:50 -0500

    feat: kofi a tag

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,15 +1,22 @@
 <script lang="ts">
   import { tilesSolution, getIsWin, isGame, tiles } from "$lib/refs.svelte";
-  import GitHub from "$lib/components/GitHub.svelte";
   import { newEditor } from "$lib/shared.svelte";
+
+  import Funding from "./Funding.svelte";
 </script>
 
 <header class="flex items-center justify-center">
-  <GitHub />
+  <div class="flex flex-1 justify-start gap-1 p-1">
+    <Funding />
+  </div>
 
-  {#if isGame.v}
-    <span class="pt-2.5">{getIsWin() ? "You win!" : "Keep trying"}</span>
-  {:else}
-    <button class="-mt-1 rounded-b-lg" onclick={() => { isGame.v = true; tilesSolution.v = tiles.v; newEditor(); }}>Play</button>
-  {/if}
+  <div class="flex flex-1 justify-center">
+    {#if isGame.v}
+      <span class="pt-2.5">{getIsWin() ? "You win!" : "Keep trying"}</span>
+    {:else}
+      <button class="-mt-1 rounded-b-lg" onclick={() => { isGame.v = true; tilesSolution.v = tiles.v; newEditor(); }}>Play</button>
+    {/if}
+  </div>
+
+  <div class="flex flex-1 justify-end"></div>
 </header>
