ad7e9cd6cc65efd442b27b3f4f93d5be8792dab1
Jefferson Hovey <jefferson.hovey@gmail.com>
2025-03-12 23:21:59 -0400

    fix: move arrow centering

--- src/lib/components/options/GameSettings.svelte
+++ src/lib/components/options/GameSettings.svelte
@@ -44,22 +44,22 @@
     <button class="flex-1" onclick={() => { newEditor(); }}>Clear</button>
   </div>
   <div class="flex">
-    <button aria-label="Shift Left" class="flex-1" onclick={() => shiftTiles("left")}>
+    <button aria-label="Shift Left" class="flex flex-1 items-center justify-center" onclick={() => shiftTiles("left")}>
       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
         <path fill="currentColor" d="M15.41 16.59L10.83 12l4.58-4.59L14 6l-6 6l6 6l1.41-1.41z"/>
       </svg>
     </button>
-    <button aria-label="Shift Right" class="flex-1" onclick={() => shiftTiles("right")}>
+    <button aria-label="Shift Right" class="flex flex-1 items-center justify-center" onclick={() => shiftTiles("right")}>
       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
         <path fill="currentColor" d="M8.59 16.59L13.17 12L8.59 7.41L10 6l6 6l-6 6l-1.41-1.41z"/>
       </svg>
     </button>
-    <button aria-label="Shift Up" class="flex-1" onclick={() => shiftTiles("up")}>
+    <button aria-label="Shift Up" class="flex flex-1 items-center justify-center" onclick={() => shiftTiles("up")}>
       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
         <path fill="currentColor" d="M12 8.59l4.59 4.58L18 12l-6-6l-6 6l1.41 1.41L12 8.59z"/>
       </svg>
     </button>
-    <button aria-label="Shift Down" class="flex-1" onclick={() => shiftTiles("down")}>
+    <button aria-label="Shift Down" class="flex flex-1 items-center justify-center" onclick={() => shiftTiles("down")}>
       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
         <path fill="currentColor" d="M12 15.41L7.41 10.83L6 12l6 6l6-6l-1.41-1.41L12 15.41z"/>
       </svg>
